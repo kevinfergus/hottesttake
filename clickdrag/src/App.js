9b8a06860ccb42b4b02ff2449072bfbcc3c2da95
Kevin Fergus <kevinfergus89@gmail.com>
2020-05-13 10:36:49 -0500

    reading data from firebase

--- clickdrag/src/App.js
+++ clickdrag/src/App.js
@@ -2,12 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { db } from './firebase';
+import Draggable from 'react-draggable';
+import { useObjectVal, useListVals } from 'react-firebase-hooks/database';
 
 function App() {
+	// componentDidMount() {
+	// 	try {
+	// 		db.ref('John').on('value', (snapshot) => {
+	// 			let vals = [];
+	// 			snapshot.forEach((snap) => vals.push(snap));
+	// 			console.log('vals', vals);
+	// 		});
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// }
+	const [ position, loading, error ] = useObjectVal(db.ref('Postition'));
+	if (loading) {
+		return <div>Loading</div>;
+	}
+	if (error) {
+		return <div>Ooops!</div>;
+	}
+
+	console.log('host', position);
+
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
+				{/* <Draggable position={this.state.position}>
+					<div className="helloworld">Hello World</div>
+				</Draggable> */}
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
--- clickdrag/src/firebase.js
+++ clickdrag/src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 const config = {
-	apiKey: 'AIzaSyDx900bEhFzPhnKIUnHUyy-TaWRQnNPu04',
-	authDomain: 'hottake-85bb4.firebaseapp.com',
-	databaseURL: 'https://hottake-85bb4.firebaseio.com',
-	projectId: 'hottake-85bb4',
-	storageBucket: 'hottake-85bb4.appspot.com',
-	messagingSenderId: '515581184089',
-	appId: '1:515581184089:web:060c709ceab01b4577c948',
-	measurementId: 'G-7SXTTFVT1Z'
+	apiKey: 'AIzaSyAkB0IcCczv1WaEkvzSigwEVBfoEDPHQLw',
+	authDomain: 'grouphang-258f0.firebaseapp.com',
+	databaseURL: 'https://grouphang-258f0.firebaseio.com',
+	projectId: 'grouphang-258f0',
+	storageBucket: 'grouphang-258f0.appspot.com',
+	messagingSenderId: '1099355694480',
+	appId: '1:1099355694480:web:c6da36b5250772cdf1bf15',
+	measurementId: 'G-L9RRLVGJ44'
 };
 export const fire = firebase.initializeApp(config);
 export const db = fire.database();
